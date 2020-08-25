29bfedee75029eff7cb1d47ec0a809f4ac9719eb
Behnam Sheykhe <learndlt5@gmail.com>
2020-08-25 10:43:31 +0430

    Done

---
+++ app.js
@@ -0,0 +1,6 @@
+const lionBtn = document.querySelector(".lion")
+const bearBtn = document.querySelector(".bear")
+const wolfBtn = document.querySelector(".wolf")
+const lionAudio = new Audio('./sounds/lion.mp3');
+const bearAudio = new Audio('./sounds/bear.mp3');
+const wolfAudio = new Audio('./sounds/wolf.mp3');
